45366f325d9b15b425a101ddfe0f7d63f2294c46
SahakK <sahak.keshishyan.dev@gmail.com>
2025-03-13 06:09:32 +0400

    init project, get and show posts

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { Posts } from './Posts';
+import './App.css';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog &apos;em Ipsum</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
